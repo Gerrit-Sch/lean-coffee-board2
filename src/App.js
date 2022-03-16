19fee53b8c5ee4ae5455395d694e2a7e52caa24f
Gerrit Schulz-Telschow <gerrit.schulz.telschow@gmail.com>
2022-03-16 16:51:28 +0100

    Add db

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
-import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Entry from './components/Entry';
+import EntryForm from './components/EntryForm';
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then(res => res.json());
 
 export default function App() {
-  const [entries, setEntries] = useState([]);
+  const {
+    data: entries,
+    error: entriesError,
+    mutate: mutateEntries,
+  } = useSWR('/api/entries', fetcher, {
+    refreshInterval: 1000,
+  });
 
-  useEffect(() => {
-    getEntries();
-
-    async function getEntries() {
-      const response = await fetch('/api/entries');
-      const entries = await response.json();
-      setEntries(entries);
-    }
-  }, []);
+  if (entriesError) return <h1>Sorry, could not fetch.</h1>;
+  if (!entries) return <em>...loading...</em>;
 
   return (
     <>
       <h1>Lean Coffee Board</h1>
-      <Grid role="list">
-        {entries.map(({ text, author }, index) => (
-          <li key={index}>
+      <EntryList role="list">
+        {entries.map(({ text, author, _id }) => (
+          <li key={_id}>
             <Entry text={text} author={author} />
           </li>
         ))}
-      </Grid>
+      </EntryList>
+      <EntryForm onSubmit={handleNewEntry} />
     </>
   );
+
+  async function handleNewEntry(text) {
+    const newEntry = {
+      text,
+      author: 'Anonymous',
+      tempId: Math.random(),
+    };
+
+    mutateEntries([...entries, newEntry], false);
+
+    await fetch('/api/entries', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newEntry),
+    });
+
+    mutateEntries();
+  }
 }
 
-const Grid = styled.ul`
+const EntryList = styled.ul`
   display: grid;
   gap: 20px;
   list-style: none;
--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import ScreenReaderOnly from './ScreenReaderOnly.js';
 
 export default function EntryForm({ onSubmit }) {
@@ -7,15 +8,23 @@ export default function EntryForm({ onSubmit }) {
       <label htmlFor="text">
         <ScreenReaderOnly>Entry text</ScreenReaderOnly>
       </label>
-      <input
+      <Input
         name="text"
         id="text"
         placeholder="Just some text ..."
         type="text"
       />
-      <PlusButton id="entry-form-name">
+      <PlusButton aria-hidden="true" id="entry-form-name">
         <ScreenReaderOnly>Create new entry</ScreenReaderOnly>
-        <div aria-hidden="true">+</div>
+        <svg
+          fill="#000000"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 50 50"
+          width="30px"
+          height="30px"
+        >
+          <path d="M 25 2 C 12.309295 2 2 12.309295 2 25 C 2 37.690705 12.309295 48 25 48 C 37.690705 48 48 37.690705 48 25 C 48 12.309295 37.690705 2 25 2 z M 25 4 C 36.609824 4 46 13.390176 46 25 C 46 36.609824 36.609824 46 25 46 C 13.390176 46 4 36.609824 4 25 C 4 13.390176 13.390176 4 25 4 z M 24 13 L 24 24 L 13 24 L 13 26 L 24 26 L 24 37 L 26 37 L 26 26 L 37 26 L 37 24 L 26 24 L 26 13 L 24 13 z" />
+        </svg>
       </PlusButton>
     </Form>
   );
@@ -24,17 +33,20 @@ export default function EntryForm({ onSubmit }) {
     event.preventDefault();
     const form = event.target;
     const inputElement = form.elements.text;
+
     onSubmit(inputElement.value);
+    form.reset();
   }
 }
 
 const Form = styled.form`
   display: flex;
   justify-content: space-between;
+  
 
   input {
     width: 100%;
-    margin-right: 20px;
+    margin-right: 20px;b
   }
 `;
 
@@ -44,8 +56,16 @@ const PlusButton = styled.button`
   width: 28px;
   height: 28px;
   border: none;
+  padding: 0;
+  background-color: transparent;
+`;
 
-  div {
-    margin-top: -1px;
+const Input = styled.input`
+  border: none;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  outline: none;
+
+  :hover {
+    border-bottom: 2px solid black;
   }
 `;
